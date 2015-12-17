1270ae622eee23a05865eea2ae3f2995656446a4
Rachouan <rachouan_15@hotmail.fr>
2015-12-17 16:34:00 +0100

    update

--- dashboard/js/app.js
+++ dashboard/js/app.js
@@ -1,7 +1,8 @@
 $(function () {
 
-	var ip = '10.3.210.80';
+	var ip = '10.3.210.80:8080';
 	var id;
+	var editid;
 
 	function init () {
 
@@ -9,7 +10,7 @@ $(function () {
 
 		$("#addNew").submit(function(e) {
 
-		    var url = "http://"+ip+":8080/Ruimtes/voegRuimteToe"; // the script where you handle the form input.
+		    var url = "http://"+ip+"/Ruimtes/voegRuimteToe"; // the script where you handle the form input.
 				var formData = new FormData();
 				var data = new FormData();
 				/*jQuery.each(jQuery('#image')[0].files, function(i, file) {
@@ -29,7 +30,75 @@ $(function () {
 				    processData: false,
 				    type: 'POST',
 				    success: function(data){
-				        alert(data);
+
+								$("article#comics section.upload").removeClass("open");
+								loadRuimtes();
+				    }
+				});
+
+
+		    e.preventDefault(); // avoid to execute the actual submit of the form.
+		});
+
+		$("article#room section article.edit #editRuimteInfo").submit(function(e) {
+
+		    var url = "http://"+ip+"/Info/updateInfo/"; // the script where you handle the form input.
+				var formData = new FormData();
+				var data = new FormData();
+				/*jQuery.each(jQuery('#image')[0].files, function(i, file) {
+				    data.append('image', file);
+				});*/
+				formData.append("info_image", jQuery('#edit_info_image')[0].files[0]);
+				formData.append("info_id", editid);
+				formData.append("info_name", $('#edit_info_name').val());
+				formData.append("description_en", $('#edit_description_en').val());
+				formData.append("description_nl", $('#edit_description_nl').val());
+				formData.append("description_fr", $('#edit_description_fr').val());
+
+				jQuery.ajax({
+				    url: url,
+				    data: formData,
+				    cache: false,
+				    contentType: false,
+				    processData: false,
+				    type: 'POST',
+				    success: function(data){
+
+								$("article#room section article.edit").removeClass("open");
+								loadInfo();
+				    }
+				});
+
+
+		    e.preventDefault(); // avoid to execute the actual submit of the form.
+		});
+
+		$("#editRuimte").submit(function(e) {
+
+		    var url = "http://"+ip+"/Ruimtes/updateRuimte"; // the script where you handle the form input.
+				var formData = new FormData();
+				var data = new FormData();
+				/*jQuery.each(jQuery('#image')[0].files, function(i, file) {
+				    data.append('image', file);
+				});*/
+				formData.append("image", jQuery('#edit_image')[0].files[0]);
+				formData.append("r_id", id);
+				formData.append("name", $('#edit_name').val());
+				formData.append("comic_en", $('#edit_comic_en').val());
+				formData.append("comic_nl", $('#edit_comic_nl').val());
+				formData.append("comic_fr", $('#edit_comic_fr').val());
+
+				jQuery.ajax({
+				    url: url,
+				    data: formData,
+				    cache: false,
+				    contentType: false,
+				    processData: false,
+				    type: 'POST',
+				    success: function(data){
+
+								$("article#comics section.edit").removeClass("open");
+								loadRuimtes("en");
 				    }
 				});
 
@@ -40,7 +109,7 @@ $(function () {
 
 		$("#addRuimteInfo").submit(function(e) {
 
-		    var url = "http://"+ip+":8080/Info/voegInfoToeAanRuimte"; // the script where you handle the form input.
+		    var url = "http://"+ip+"/Info/voegInfoToeAanRuimte"; // the script where you handle the form input.
 				var formData = new FormData();
 				var data = new FormData();
 
@@ -60,7 +129,8 @@ $(function () {
 				    processData: false,
 				    type: 'POST',
 				    success: function(data){
-				        alert(data);
+							$("article#room section article.add").removeClass("open");
+							loadInfo();
 				    }
 				});
 
@@ -79,6 +149,23 @@ $(function () {
 				}else{
 					$(this).addClass('open');
 					$("article#room section article.add").addClass("open");
+				}
+		});
+
+		$("article#room section article.edit aside ul li span").on("click",function (e) {
+				e.preventDefault();
+				var tab = $(this).parent();
+
+
+				if(tab.hasClass("open")){
+					tab.removeClass("open");
+				}else{
+					$("article#room section article.edit aside ul li").each(function (key,val) {
+						if( tab !== val){
+								$(val).removeClass('open');
+						}
+					});
+					tab.addClass("open");
 				}
 		});
 
@@ -161,7 +248,7 @@ $(function () {
 			var fd = new FormData(document.getElementById($(this)[0]));
 			console.log($(fd).serialize());
             $.ajax({
-              url: "http://10.3.210.104:8080/Ruimtes/voegRuimteToe",
+              url: "http://10.3.210.104/Ruimtes/voegRuimteToe",
               type: "POST",
               data: fd,
               processData: false,  // tell jQuery not to process the data
@@ -177,13 +264,19 @@ $(function () {
 	function loadRuimtes (lang) {
 
 
+		$("article#comics section").scrollLeft(0);
+		$("article#comics section.comics").each(function	(key,val) {
+			if (!$(val).hasClass('upload') && !$(val).hasClass('edit')) {
+				$(val).remove();
+			}
+		});
 
-		$.get( "http://"+ip+":8080/Ruimtes/getAll?lang="+lang, function( data ) {
+		$.get( "http://"+ip+"/Ruimtes/getAll?lang="+lang, function( data ) {
 
 			$(data).each(function (key,val) {
 				console.log(val);
 
-				$("<section/>").addClass("comics").addClass("comics").html("<header><h1>"+val.name+"</h1><h2>"+val.comic+"</h2><a href='' class='edit'></a></header><nav><ul><li><a href='' id='"+val.id+"'><span class='icon enter'></span><span>Enter</span></a></li><li><a href=''><span class='icon edit'></span><span>Edit</span></a></li><li><a href='' id='"+val.id+"'><span class='icon trash'></span><span>Delete</span></a></li></ul></nav>").appendTo("article#comics");
+				$("<section/>").addClass("comics").addClass("comics").html("<header><h1>"+val.name+"</h1><h2>"+val.comic+"</h2><a href='' class='edit'></a></header><nav><ul><li><a href='' id='"+val.id+"'><span class='icon enter'></span><span>Enter</span></a></li><li><a href=''  id='"+val.id+"'><span class='icon edit'></span><span>Edit</span></a></li><li><a href='' id='"+val.id+"'><span class='icon trash'></span><span>Delete</span></a></li></ul></nav>").appendTo("article#comics");
 
 
 			});
@@ -196,7 +289,8 @@ $(function () {
 
 	function loadInfo (lang) {
 
-		console.log("hey");
+		$("article#room section").scrollTop(0);
+
 
 		$("article#room section article").each(function	(key,val) {
 			if(!$(val).hasClass('add') && !$(val).hasClass('edit')){
@@ -204,7 +298,7 @@ $(function () {
 			}
 		});
 
-		$.get( "http://"+ip+":8080/Info/getAlleInfoPerLang?r_id="+id+"lang="+lang, function( data ) {
+		$.get( "http://"+ip+"/Info/getAlleInfoPerLang?r_id="+id+"lang="+lang, function( data ) {
 
 			$(data).each(function (key,val) {
 				console.log(val);
@@ -218,37 +312,50 @@ $(function () {
 				e.preventDefault();
 				console.log("EDIT");
 				var edit = $(this);
+				editid = edit.attr('id');
 
 				if($("article#room section article.edit").hasClass('open')){
 					$("article#room section article.edit").removeClass('open');
 				}else {
 					$("article#room section article.edit").addClass('open');
+
+					$.get( "http://"+ip+"/Info/getInfoMetAlleTalen?i_id="+editid, function( data ) {
+						console.log(data);
+
+						$("article#room section article.edit #edit_info_name").val(data[0].name);
+
+						$(data).each(function (key,val) {
+								switch (val.lang.toLowerCase()) {
+									case 'en':
+									$("article#room section article.edit #edit_description_en").val(val.info);
+										break;
+
+									case 'nl':
+									$("article#room section article.edit #edit_description_nl").val(val.info);
+										break;
+
+									case 'fr':
+									$("article#room section article.edit #edit_description_fr").val(val.info);
+										break;
+									default:
+
+								}
+						});
+
+
+					});
+
+
+
 				}
 
-					$.get( "http://"+ip+":8080/Info/getInfo?i_id="+edit.attr('id'), function( data ) {
-						console.log(data);
-					});
+
 			});
 
 		});
 
 
-		$("article#room section article.edit aside ul li span").on("click",function (e) {
-				e.preventDefault();
-				var tab = $(this).parent();
 
-
-				if(tab.hasClass("open")){
-					tab.removeClass("open");
-				}else{
-					$("article#room section article.edit aside ul li").each(function (key,val) {
-						if( tab !== val){
-								$(val).removeClass('open');
-						}
-					});
-					tab.addClass("open");
-				}
-		});
 
 	}
 
@@ -303,7 +410,7 @@ $(function () {
 					case "icon trash":
 
 						console.log($(this).attr("id"));
-						$.post( "http://"+ip+":8080/Ruimtes/verwijderRuimte",{id:$(this).attr("id")}, function( data ) {
+						$.post( "http://"+ip+"/Ruimtes/verwijderRuimte",{id:$(this).attr("id")}, function( data ) {
 						  console.log(data);
 						});
 
@@ -315,6 +422,44 @@ $(function () {
 						$("article#room header h1").text(comic.find('header h1').text()+' room');
 						loadInfo("en");
 						console.log(id);
+
+					break;
+
+					case "icon edit":
+
+						id = $(this).attr("id");
+						console.log(id);
+						if ($("article#comics section.edit").hasClass("open")) {
+							$("article#comics section.edit").removeClass("open");
+						}else {
+							$("article#comics section.edit").addClass("open");
+						}
+
+						$.get( "http://"+ip+"/Ruimtes/getInfoMetAlleTalen?r_id="+id, function( data ) {
+							console.log(data);
+
+							$("#edit_name").val(data[0].name);
+
+							$(data).each(function (key,val) {
+									switch (val.lang.toLowerCase()) {
+										case 'en':
+										$("#edit_comic_en").val(val.info);
+											break;
+
+										case 'nl':
+										$("#edit_comic_nl").val(val.info);
+											break;
+
+										case 'fr':
+										$("#edit_comic_fr").val(val.info);
+											break;
+										default:
+
+									}
+							});
+
+
+						});
 
 					break;
 
